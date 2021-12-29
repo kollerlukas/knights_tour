06dd08149e47dc6cbf8f4f5fda02e314660d1584
kollerlukas <lukaskoller6@gmail.com>
2021-12-29 11:57:29 +0100

    added comments to python script

--- compute_path.py
+++ compute_path.py
@@ -3,17 +3,19 @@ import sys
 # sys.setrecursionlimit(10000)
 
 # parameter
-n,m = 8,8
-s_0 = (1,1)
-s_nm = (3,2)
+n,m = 8,8 # size of the field
+s_0 = (1,1) # start point
+s_nm = (3,2) # end point
 
-board = {(i+1,j+1) for i in range(n) for j in range(m)}
+board = {(i+1,j+1) for i in range(n) for j in range(m)} # build board
 
+# return the number of unvisited neighbors of the square si.
 def num_neighbors(si,b):
   i,j = si
   neighbors = {(i+1,j+2),(i-1,j+2),(i+1,j-2),(i-1,j-2),(i+2,j+1),(i-2,j+1),(i+2,j-1),(i-2,j-1)}
   return len(neighbors.intersection(b))
 
+# return a list of all valid steps from a square si
 def valid_steps(si,b):
   i,j = si
   steps = [(i+1,j+2),(i-1,j+2),(i+1,j-2),(i-1,j-2),(i+2,j+1),(i-2,j+1),(i+2,j-1),(i-2,j-1)]
@@ -21,6 +23,7 @@ def valid_steps(si,b):
   valid_steps = sorted(valid_steps, key=lambda si: num_neighbors(si,b))
   return valid_steps
 
+# recursively compute a path from si to sj using the unvisited squares in b
 def compute_path(si,sj,b):
   if b == {} or not si in b or not sj in b:
     return None
@@ -35,6 +38,7 @@ def compute_path(si,sj,b):
         return p
     return None
 
+# convert a sequence of squares to a nice matrix representation
 def path_to_matrix(n,m,p):
   m = [[0 for _ in range(m)] for _ in range(n)]
   for k, sk in enumerate(p):
@@ -42,7 +46,9 @@ def path_to_matrix(n,m,p):
     m[i-1][j-1] = k+1
   return m
 
+# compute the paths
 p = compute_path(s_0,s_nm,board) 
 m = path_to_matrix(n,m,p)
+# print result
 for r in m:
   print(r)
